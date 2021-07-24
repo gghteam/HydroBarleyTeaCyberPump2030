d8f3cb3c1b5771a34e750c6ffb5b5f09a4d33b9e
KimM1nSang <minsang1008@naver.com>
2021-07-25 04:39:51 +0900

    세이브를 구현을 했는데 이게 올바르게 올라갈까요?

---
+++ HydroTeaPump/Assets/01_Scripts/Data/SaveData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData
+{
+    int TestInt = 100;
+    string TestString = "테스트용";
+}
---
+++ HydroTeaPump/Assets/01_Scripts/Save/GameSave.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameSave : MonoBehaviour
+{
+    //싱글톤====================
+    static GameObject _container;
+    static GameObject Container
+    {
+        get
+        {
+            return _container;
+        }
+    }
+    static GameSave _instance;
+    public static GameSave Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _container = new GameObject();
+                _container.name = "SaveGame";
+                _instance = _container.AddComponent(typeof(GameSave)) as GameSave;
+                DontDestroyOnLoad(_container);
+            }
+            return _instance;
+        }
+    }
+    // =================================================
+    public string GameDataFileName = ".json";
+
+    private string filePath = "";
+
+
+    private SaveData _data;
+
+    public SaveData data
+    {
+        get
+        {
+            if (_data == null)
+            {
+                LoadGameData();
+                SaveGameData();
+            }
+            return _data;
+        }
+    }
+    private void Awake()
+    {
+        filePath = string.Concat(Application.persistentDataPath, GameDataFileName);
+        Debug.Log(filePath);
+    }
+
+    public void LoadGameData()
+    {
+        if (File.Exists(filePath))
+        {
+            Debug.Log("불러오기");
+            string FromJsonData = File.ReadAllText(filePath);
+
+            _data = JsonUtility.FromJson<SaveData>(FromJsonData);
+        }
+        else
+        {
+            Debug.Log("새로운 파일 생성");
+            _data = new SaveData();
+        }
+
+    }
+
+    public void SaveGameData()
+    {
+        string ToJsonData = JsonUtility.ToJson(data, true);
+        Debug.Log("저장완료");
+
+        File.WriteAllText(filePath, ToJsonData);
+    }
+}
