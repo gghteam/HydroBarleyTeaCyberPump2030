8ef4a9a19eeec9806b6066f2468b1776a01196b8
HomeHan <wooyeophan@gmail.com>
2021-07-25 22:14:42 +0900

    오이런 컴파일 에러가 있는 상태로 올렸었네요

--- HydroTeaPump/Assets/01_Scripts/UI/Main/MainMenuUI.cs
+++ HydroTeaPump/Assets/01_Scripts/UI/Main/MainMenuUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UIManagement.Button;
+using UI.Management.Button;
 
 public class MainMenuUI : MonoBehaviour
 {
