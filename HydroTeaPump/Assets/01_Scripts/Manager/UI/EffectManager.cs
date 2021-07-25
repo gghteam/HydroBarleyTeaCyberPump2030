d21ebb5d5c5a764477d66be80e0c45e49609ec09
HomeHan <wooyeophan@gmail.com>
2021-07-25 12:19:46 +0900

    UIManager 를 만들긴 했스빈다. Readme 파일로 사용법 올려두겟슴

--- HydroTeaPump/Assets/01_Scripts/Manager/UI/EffectManager.cs
+++ HydroTeaPump/Assets/01_Scripts/Manager/UI/EffectManager.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class EffectManager : MonoBehaviour
 {
+    // 잠만 이거 뭘 해야 할지 모르겠어서 대충만 개발해둠
+
     // Start is called before the first frame update
     void Start()
     {
