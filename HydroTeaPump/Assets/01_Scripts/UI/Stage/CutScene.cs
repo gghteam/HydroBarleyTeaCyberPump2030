5a4a0651ab9135d37278065c1b71fc9c6f022115
KimM1nSang <minsang1008@naver.com>
2021-07-27 22:05:35 +0900

    리워드 씬깔끔하게 바꿨음

--- HydroTeaPump/Assets/01_Scripts/UI/Stage/CutScene.cs
+++ HydroTeaPump/Assets/01_Scripts/UI/Stage/CutScene.cs
@@ -18,7 +18,7 @@ public class CutScene : MonoBehaviour
     private void Start()
     {
         gameObject.SetActive(false);
-        explainText = transform.GetChild(2).GetComponent<Text>();
+        explainText = transform.GetChild(3).GetComponent<Text>();
 
         textManager = GameObject.Find("TextManager").GetComponent<TextManager>();
 
@@ -46,20 +46,20 @@ public class CutScene : MonoBehaviour
         
         gameObject.SetActive(true);
         SetChildAlpha();
-        Vector3 prevScale = transform.GetChild(0).localScale;
-        Vector3 prevTextScale = transform.GetChild(2).localScale;
+        Vector3 prevScale = transform.GetChild(1).localScale;
+        Vector3 prevTextScale = transform.GetChild(3).localScale;
 
         Sequence seq = DOTween.Sequence();
         //뒷 이미지
-        transform.GetChild(0).localScale -= transform.localScale / 2;
-        seq.Append(transform.GetChild(0).DOScale(prevScale, .5f).SetEase(Ease.OutBack));
+        transform.GetChild(1).localScale -= transform.localScale / 2;
+        seq.Append(transform.GetChild(1).DOScale(prevScale, .5f).SetEase(Ease.OutBack));
         //설명 이미지
-        seq.Join(transform.GetChild(1).GetComponent<Image>().DOFade(1, .2f).SetDelay(.3f));
+        seq.Join(transform.GetChild(2).GetComponent<Image>().DOFade(1, .2f).SetDelay(.3f));
 
         //텍스트
-        transform.GetChild(2).localScale -= transform.localScale / 2;
-        seq.Join(transform.GetChild(2).GetComponent<Text>().DOFade(1, 1f));
-        seq.Join(transform.GetChild(2).DOScale(prevTextScale, .4f).SetEase(Ease.OutBack));
+        transform.GetChild(3).localScale -= transform.localScale / 2;
+        seq.Join(transform.GetChild(3).GetComponent<Text>().DOFade(1, 1f));
+        seq.Join(transform.GetChild(3).DOScale(prevTextScale, .4f).SetEase(Ease.OutBack));
 
         seq.OnComplete(()=> { isPlaying = false; });
     }
@@ -75,7 +75,7 @@ public class CutScene : MonoBehaviour
     }
     private void SetChildAlpha()
     {
-        transform.GetChild(1).GetComponent<Image>().color = new Color(1,1,1,0) ;
-        transform.GetChild(2).GetComponent<Text>().color = new Color(1,1,1,0) ;
+        transform.GetChild(2).GetComponent<Image>().color = new Color(1,1,1,0) ;
+        transform.GetChild(3).GetComponent<Text>().color = new Color(1,1,1,0) ;
     }
 }
