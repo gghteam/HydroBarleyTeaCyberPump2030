ba3e0e6cc6f49054d305597a3972407f9516794e
HomeHan <wooyeophan@gmail.com>
2021-07-27 23:10:17 +0900

    제작 에니메이션

--- HydroTeaPump/Assets/01_Scripts/UI/Anim/Craft/CraftAnim.cs
+++ HydroTeaPump/Assets/01_Scripts/UI/Anim/Craft/CraftAnim.cs
@@ -18,17 +18,28 @@ public class CraftAnim : MonoBehaviour
 
     void Start()
     {
-        images[0].sprite = craft.GetTable()[0].image.sprite;
-        images[1].sprite = craft.GetTable()[1].image.sprite;
-        images[2].sprite = craft.GetTable()[2].image.sprite;
+
+
+        images[0].gameObject.SetActive(false);
+        images[1].gameObject.SetActive(false);
+        images[2].gameObject.SetActive(false);
     }
     
 
     public void Animation()
     {
+        images[0].sprite = craft.GetTable()[0].image.sprite;
+        images[1].sprite = craft.GetTable()[1].image.sprite;
+        images[2].sprite = craft.GetTable()[2].image.sprite;
+        
+        images[0].gameObject.SetActive(true);
+        images[1].gameObject.SetActive(true);
+        images[2].gameObject.SetActive(true);
+
+        animator.SetBool("clear", craft.Success());
+
         animator.Play("Craft");
         animator.SetTrigger("Craft");
 
-        animator.SetBool("clear", craft.Success());
     }
 }
