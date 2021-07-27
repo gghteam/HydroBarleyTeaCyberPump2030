0a0735cd4f6122b1223453e98ee237783f44a056
KimM1nSang <minsang1008@naver.com>
2021-07-27 12:15:06 +0900

    죽은자 소생! 없어졌던 내용을 복구한다

---
+++ HydroTeaPump/Assets/01_Scripts/Manager/Rythm/Stage/StageManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] StagePrefabs;
+
+    private void Start()
+    {
+        Instantiate(StagePrefabs[/*GameManager.Instance.currentStage*/0]);
+    }
+}
