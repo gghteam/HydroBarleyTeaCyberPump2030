30502a59d51ebc09e907e8b8cfcfc7c9619967c1
HomeHan <wooyeophan@gmail.com>
2021-07-27 15:13:19 +0900

    널 수정해드립니다.

--- HydroTeaPump/Assets/01_Scripts/Rythm/Move/PlayerMove/RhythmPlayerInput.cs
+++ HydroTeaPump/Assets/01_Scripts/Rythm/Move/PlayerMove/RhythmPlayerInput.cs
@@ -10,17 +10,20 @@ public class RhythmPlayerInput : MonoBehaviour
     public bool left  { get; private set; }
     public bool right { get; private set; }
 
-    [SerializeField] private KeyCode upKey;
-    [SerializeField] private KeyCode downKey;
-    [SerializeField] private KeyCode leftKey;
-    [SerializeField] private KeyCode rightKey;
+    private KeyCode upKey    = KeyCode.UpArrow;
+    private KeyCode downKey  = KeyCode.DownArrow;
+    private KeyCode leftKey  = KeyCode.LeftArrow;
+    private KeyCode rightKey = KeyCode.RightArrow;
 
     private void Awake()
     {
-        upKey    = OptionManager.GetSettings().moveUp;
-        downKey  = OptionManager.GetSettings().moveDown;
-        leftKey  = OptionManager.GetSettings().moveLeft;
-        rightKey = OptionManager.GetSettings().moveRight;
+        if (OptionManager.GetSettings() != null)
+        {
+            upKey = OptionManager.GetSettings().moveUp;
+            downKey = OptionManager.GetSettings().moveDown;
+            leftKey = OptionManager.GetSettings().moveLeft;
+            rightKey = OptionManager.GetSettings().moveRight;
+        }
     }
 
     private void Update()
